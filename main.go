27fbba6883b00836dad8ef3d608d6cdbd0da322a
Dung Dinh Nguyen Anh (VN) <Dung.DinhNA@homecredit.vn>
2023-12-27 10:48:37 +0700

    Move all seive code to a separate package

--- main.go
+++ main.go
@@ -3,47 +3,23 @@ package main
 import (
 	"fmt"
 	"math"
+	"time"
+
+	"homecredit.vn/prime-go/sieve"
 )
 
 var prime_count int64
 
 func searchPrime(N int64) {
 
-	prime_bits := make([]byte, N/16+1)
-
-	//
-	// initially, set all odd numbers to primes
-	//
-	for i := 0; i < len(prime_bits); i++ {
-		prime_bits[i] = 0b11111111
-	}
+	sieve.Init(N)
+	sieve.Begin()
 
 	nsqrt := int64(math.Sqrt(float64(N)))
 
-	// current bit and byte position so that we can "move" along with i each step
-	pos_byte := int(0)
-	pos_bit := int(0)
+	for i := int64(3); i <= nsqrt; i += 2 {
 
-	for i := int64(3); i < nsqrt; i++ {
-		byte_i := byte(0b11111111)
-		switch pos_bit {
-		case 0:
-			byte_i = (byte)(prime_bits[pos_byte] & 0b00000001)
-		case 1:
-			byte_i = (byte)(prime_bits[pos_byte] & 0b00000010)
-		case 2:
-			byte_i = (byte)(prime_bits[pos_byte] & 0b00000100)
-		case 3:
-			byte_i = (byte)(prime_bits[pos_byte] & 0b00001000)
-		case 4:
-			byte_i = (byte)(prime_bits[pos_byte] & 0b00010000)
-		case 5:
-			byte_i = (byte)(prime_bits[pos_byte] & 0b00100000)
-		case 6:
-			byte_i = (byte)(prime_bits[pos_byte] & 0b01000000)
-		case 7:
-			byte_i = (byte)(prime_bits[pos_byte] & 0b10000000)
-		}
+		byte_i := sieve.Get()
 
 		//
 		// Found a real prime! process to marking
@@ -51,75 +27,26 @@ func searchPrime(N int64) {
 		if byte_i != 0 {
 			for j := i * i; j <= N; j += i {
 				if j%2 != 0 {
-					jj := (j - 3) / 2
-
-					b := int(jj / 8)
-					bi := int(jj % 8)
-
-					switch bi {
-					case 0:
-						prime_bits[b] &= 0b11111110
-					case 1:
-						prime_bits[b] &= 0b11111101
-					case 2:
-						prime_bits[b] &= 0b11111011
-					case 3:
-						prime_bits[b] &= 0b11110111
-					case 4:
-						prime_bits[b] &= 0b11101111
-					case 5:
-						prime_bits[b] &= 0b11011111
-					case 6:
-						prime_bits[b] &= 0b10111111
-					case 7:
-						prime_bits[b] &= 0b01111111
-					}
+					sieve.Mark(j)
 				}
 			}
 		}
 
-		pos_bit++
-		if pos_bit == 8 {
-			pos_bit = 0
-			pos_byte++
-		}
+		sieve.Next()
 	}
 
 	fmt.Println("Done marking. Now counting primes!")
 	prime_count = 1
-	pos_byte = 0
-	pos_bit = 0
-	byte_i := byte(0b11111111)
-	for i := int64(3); i <= N; i += 2 {
-		// extract bit at position corresponding to interger i
-		switch pos_bit {
-		case 0:
-			byte_i = prime_bits[pos_byte] & 0b00000001
-		case 1:
-			byte_i = prime_bits[pos_byte] & 0b00000010
-		case 2:
-			byte_i = prime_bits[pos_byte] & 0b00000100
-		case 3:
-			byte_i = prime_bits[pos_byte] & 0b00001000
-		case 4:
-			byte_i = prime_bits[pos_byte] & 0b00010000
-		case 5:
-			byte_i = prime_bits[pos_byte] & 0b00100000
-		case 6:
-			byte_i = prime_bits[pos_byte] & 0b01000000
-		case 7:
-			byte_i = prime_bits[pos_byte] & 0b10000000
-		}
 
+	sieve.Begin()
+	for i := int64(3); i <= N; i += 2 {
+		byte_i := sieve.Get()
 		if byte_i != 0 {
 			prime_count++ //primes.add(i);
+			//fmt.Print(i, " ")
 		}
 
-		pos_bit++
-		if pos_bit == 8 {
-			pos_bit = 0
-			pos_byte++
-		}
+		sieve.Next()
 	}
 
 }
@@ -145,6 +72,8 @@ https://t5k.org/howmany.html
 */
 
 func main() {
-	searchPrime(10000)
-	fmt.Println(prime_count)
+	start := time.Now()
+	searchPrime(1000000000)
+	//searchPrime(100000000000)
+	fmt.Println("Found ", prime_count, " primes in ", time.Since(start))
 }
---
+++ sieve/sieve.go
@@ -0,0 +1,93 @@
+package sieve
+
+var bits []byte
+
+func init() {
+
+}
+
+// Internal iterator
+var current_byte int
+var current_bit int
+
+func Begin() {
+	current_byte = 0
+	current_bit = 0
+}
+
+func Next() {
+	current_bit++
+	if current_bit == 8 {
+		current_byte++
+		current_bit = 0
+	}
+}
+
+// Initialize the sieve to store enough N int64
+func Init(N int64) {
+
+	bits = make([]byte, N/16+1)
+
+	//
+	// initially, set all bits to 1 to tell that they are primes
+	//
+	for i := 0; i < len(bits); i++ {
+		bits[i] = 0b11111111
+	}
+}
+
+// Retrieve current element.
+func Get() byte {
+	var v byte
+	switch current_bit {
+	case 0:
+		v = (byte)(bits[current_byte] & 0b00000001)
+	case 1:
+		v = (byte)(bits[current_byte] & 0b00000010)
+	case 2:
+		v = (byte)(bits[current_byte] & 0b00000100)
+	case 3:
+		v = (byte)(bits[current_byte] & 0b00001000)
+	case 4:
+		v = (byte)(bits[current_byte] & 0b00010000)
+	case 5:
+		v = (byte)(bits[current_byte] & 0b00100000)
+	case 6:
+		v = (byte)(bits[current_byte] & 0b01000000)
+	case 7:
+		v = (byte)(bits[current_byte] & 0b10000000)
+	}
+	return v
+}
+
+// Mark position idx as non prime
+func Mark(n int64) {
+
+	// b:	0                    1                      2
+	// idx:	0 1 2 3  4  5  6  7  8  9 10 11 12 13 14 15 16 .... (idx)
+	// n:	3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 .... (idx*2+3) = ii  => ii = ( idx - 3 ) / 2
+
+	var ii int64 = (n - 3) / 2
+
+	b := int(ii / 8)
+	bi := int(ii % 8)
+
+	switch bi {
+	case 0:
+		bits[b] &= 0b11111110
+	case 1:
+		bits[b] &= 0b11111101
+	case 2:
+		bits[b] &= 0b11111011
+	case 3:
+		bits[b] &= 0b11110111
+	case 4:
+		bits[b] &= 0b11101111
+	case 5:
+		bits[b] &= 0b11011111
+	case 6:
+		bits[b] &= 0b10111111
+	case 7:
+		bits[b] &= 0b01111111
+	}
+}
