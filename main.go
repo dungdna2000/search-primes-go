7f23d53348a9375f4ef87d63d8ea475e762699a0
Đinh Nguyễn Anh Dũng <dungdna2000@gmail.com>
2023-12-29 13:58:00 +0700

    Use byte lookup rather than switch in sieve.go

--- main.go
+++ main.go
@@ -11,59 +11,42 @@ import (
 var prime_count int64
 var SV sieve.Sieve
 
-var mark_i int64
-var mark_j int64
+var p int64         // the current found prime
+var composite int64 // the composite to be marked
 var count_i int64
 var finished bool
 
 func searchPrime(N int64) {
 
-	fmt.Println("D Initializing sieve with size ", N)
 	SV.Init(N)
-
 	SV.Begin()
 
 	nsqrt := int64(math.Sqrt(float64(N)))
 
-	fmt.Println("D Start marking  sqrt(N)=", nsqrt)
+	//fmt.Println("D Start marking  sqrt(N)=", nsqrt)
 
-	mark_j = 0
+	composite = 0
+	prime_count = 2
 
 	var d int64 = 4
-	for mark_i = 5; mark_i <= nsqrt; mark_i += d {
-		byte_i := SV.Get()
-
-		if byte_i != 0 {
-
-			var _2i int64 = 2 * mark_i
-			for mark_j = mark_i * mark_i; mark_j <= N; mark_j += _2i {
-				SV.Mark(mark_j)
+	for p = 5; p <= nsqrt; p += d {
+		if SV.Get() != 0 {
+			var _2i int64 = 2 * p
+			for composite = p * p; composite <= N; composite += _2i {
+				SV.Mark(composite)
 			}
-
-			// Use 2 routines to hopefully speed things up!
-			// isquare := i * i
-			// d := (N - isquare) / 2
-			// m := isquare + d
-			// m = m - m%i // m%i is to make sure m is at the correct (i-th step)
-
-			// wait_mark.Add(2)
-			// go mark(1, isquare, m, i)
-			// go mark(2, m+i, N, i)
-
-			// wait_mark.Wait()
+			//prime_count++
 		}
 
 		SV.Next()
 		d = flip24(d)
 	}
 
-	fmt.Println("D Marking done. Counting primes...")
-
 	prime_count = 2
+	d = 4
 	SV.Begin()
 	for count_i = 5; count_i <= N; count_i += d {
-		byte_i := SV.Get()
-		if byte_i != 0 {
+		if SV.Get() != 0 {
 			prime_count++
 		}
 		SV.Next()
@@ -85,10 +68,10 @@ https://t5k.org/howmany.html
 06	1,000,000	        	78,498
 07	10,000,000          	664,579
 08	100,000,000         	5,761,455
-09	1,000,000,000       	50,847,534			9s
-10	10,000,000,000      	455,052,511         1m42s
-11	100,000,000,000     	4,118,054,813
-12	1,000,000,000,000   	37,607,912,018		(300m)
+09	1,000,000,000       	50,847,534			~7s
+10	10,000,000,000      	455,052,511         ~1m24s
+11	100,000,000,000     	4,118,054,813		~0m
+12	1,000,000,000,000   	37,607,912,018		<< this is our TARGET
 13	10,000,000,000,000  	346,065,536,839
 14	100,000,000,000,000		3,204,941,750,802
 15	1,000,000,000,000,000	29,844,570,422,669
@@ -104,43 +87,65 @@ func flip24(d int64) int64 {
 	return 4
 }
 
-// func main() {
-// 	N := 100
+func test_case(N int64, expected int64) {
+	searchPrime(N)
+	fmt.Print("Test N=", N, " ... ")
+	if prime_count == expected {
+		fmt.Println("OK")
+	} else {
+		fmt.Println("FAILED! Actual = ", prime_count, ", Expected = ", expected)
+	}
+}
 
-// 	var d int
-// 	d = 4
-// 	for i := 5; i < N; i += d {
-// 		fmt.Printf("%02d ", i)
-// 		d = flipflop(d, 2, 4)
-// 	}
-// 	fmt.Println()
-
-// 	d = 4
-// 	for i := 5; i < N; i += d {
-
-// 		t := (i - 5) / 3
-// 		if (i-5)%3 == 2 {
-// 			t += 1
-// 		}
-
-// 		fmt.Printf("%02d ", t)
-// 		d = flipflop(d, 2, 4)
-// 	}
-// 	fmt.Println()
-
-// }
+func unit_test() {
+	test_case(100, 25)
+	test_case(1000, 168)
+	test_case(10000, 1229)
+	test_case(100000, 9592)
+	test_case(1000000, 78498)
+	test_case(10000000, 664579)
+	test_case(100000000, 5761455)
+	test_case(B, 50847534)
+}
 
 func main() {
 	start := time.Now()
 
 	finished = false
+	unit_test()
+	//go test_case(10*B, 455052511)
+	//go test_case(1000*B, 4118054813)
 
-	searchPrime(1000)
+	for !finished {
+		fmt.Println(time.Since(start), " p: ", p, ">", composite, " count: ", count_i)
+		time.Sleep(2 * time.Second)
+	}
 
-	// for !finished {
-	// 	fmt.Println(time.Since(start), ": P: ", mark_i, ">", mark_j, " count: ", count_i)
-	// 	time.Sleep(2 * time.Second)
-	// }
-
-	fmt.Println("Found ", prime_count, " primes in ", time.Since(start))
+	fmt.Println("Total time:", time.Since(start))
 }
+
+// func main1() {
+// 	var N int64 = 100
+// 	var d int64 = 4
+// 	var p int64
+
+// 	for p = 5; p < N; p += d {
+// 		fmt.Printf("%02d ", p)
+// 		d = flip24(d)
+// 	}
+// 	fmt.Println()
+
+// 	d = 4
+// 	for p = 5; p < N; p += d {
+
+// 		t := (p - 5) / 3
+// 		if (p-5)%3 == 2 {
+// 			t += 1
+// 		}
+
+// 		fmt.Printf("%02d ", t)
+// 		d = flip24(d)
+// 	}
+// 	fmt.Println()
+
+// }
--- sieve/sieve.go
+++ sieve/sieve.go
@@ -9,7 +9,31 @@ type Sieve struct {
 	current_bit  int
 }
 
+var mask_get [8]byte
+var mask_mark [8]byte
+
 func init() {
+	mask_get = [8]byte{
+		0b00000001,
+		0b00000010,
+		0b00000100,
+		0b00001000,
+		0b00010000,
+		0b00100000,
+		0b01000000,
+		0b10000000,
+	}
+
+	mask_mark = [8]byte{
+		0b11111110,
+		0b11111101,
+		0b11111011,
+		0b11110111,
+		0b11101111,
+		0b11011111,
+		0b10111111,
+		0b01111111,
+	}
 
 }
 
@@ -38,16 +62,13 @@ func (sv Sieve) String() string {
 // Initialize the sieve to store enough N int64
 func (sv *Sieve) Init(N int64) {
 
-	fmt.Println("DEBUG sieve.Init N = ", N)
+	fmt.Println("D sieve.Init N = ", N)
 
 	required_size := N/(3*8) + 1
-	fmt.Println("DEBUG required_size =", required_size)
-
 	sv.bits = make([]byte, required_size)
-
 	len_bits := len(sv.bits)
 
-	fmt.Println("DEBUG len(bits) =", len_bits)
+	fmt.Println("D sieve size =", len_bits)
 
 	//
 	// initially, set all bits to 1 to tell that they are primes
@@ -59,26 +80,29 @@ func (sv *Sieve) Init(N int64) {
 
 // Retrieve current element.
 func (sv *Sieve) Get() byte {
-	var v byte
-	switch sv.current_bit {
-	case 0:
-		v = (byte)(sv.bits[sv.current_byte] & 0b00000001)
-	case 1:
-		v = (byte)(sv.bits[sv.current_byte] & 0b00000010)
-	case 2:
-		v = (byte)(sv.bits[sv.current_byte] & 0b00000100)
-	case 3:
-		v = (byte)(sv.bits[sv.current_byte] & 0b00001000)
-	case 4:
-		v = (byte)(sv.bits[sv.current_byte] & 0b00010000)
-	case 5:
-		v = (byte)(sv.bits[sv.current_byte] & 0b00100000)
-	case 6:
-		v = (byte)(sv.bits[sv.current_byte] & 0b01000000)
-	case 7:
-		v = (byte)(sv.bits[sv.current_byte] & 0b10000000)
-	}
-	return v
+	// is this actually faster?
+	return sv.bits[sv.current_byte] & mask_get[sv.current_bit]
+
+	// var v byte
+	// switch sv.current_bit {
+	// case 0:
+	// 	v = (byte)(sv.bits[sv.current_byte] & 0b00000001)
+	// case 1:
+	// 	v = (byte)(sv.bits[sv.current_byte] & 0b00000010)
+	// case 2:
+	// 	v = (byte)(sv.bits[sv.current_byte] & 0b00000100)
+	// case 3:
+	// 	v = (byte)(sv.bits[sv.current_byte] & 0b00001000)
+	// case 4:
+	// 	v = (byte)(sv.bits[sv.current_byte] & 0b00010000)
+	// case 5:
+	// 	v = (byte)(sv.bits[sv.current_byte] & 0b00100000)
+	// case 6:
+	// 	v = (byte)(sv.bits[sv.current_byte] & 0b01000000)
+	// case 7:
+	// 	v = (byte)(sv.bits[sv.current_byte] & 0b10000000)
+	// }
+	// return v
 }
 
 // Mark position idx as non prime
@@ -89,29 +113,35 @@ func (sv *Sieve) Mark(n int64) {
 
 	// compact sieve for 2, 3
 	var ii int64 = (n - 5) / 3
-	if (n-5)%3 == 2 {
+	m := (n - 5) % 3
+	if m == 2 {
 		ii++
+	} else if m != 0 {
+		return // mark a divider of 3 (m must be either 0 or 2)> skip!
 	}
+
+	//fmt.Print(ii, " ")
 
 	b := int(ii / 8)
 	bi := int(ii % 8)
+	sv.bits[b] &= mask_mark[bi]
 
-	switch bi {
-	case 0:
-		sv.bits[b] &= 0b11111110
-	case 1:
-		sv.bits[b] &= 0b11111101
-	case 2:
-		sv.bits[b] &= 0b11111011
-	case 3:
-		sv.bits[b] &= 0b11110111
-	case 4:
-		sv.bits[b] &= 0b11101111
-	case 5:
-		sv.bits[b] &= 0b11011111
-	case 6:
-		sv.bits[b] &= 0b10111111
-	case 7:
-		sv.bits[b] &= 0b01111111
-	}
+	// switch bi {
+	// case 0:
+	// 	sv.bits[b] &= 0b11111110
+	// case 1:
+	// 	sv.bits[b] &= 0b11111101
+	// case 2:
+	// 	sv.bits[b] &= 0b11111011
+	// case 3:
+	// 	sv.bits[b] &= 0b11110111
+	// case 4:
+	// 	sv.bits[b] &= 0b11101111
+	// case 5:
+	// 	sv.bits[b] &= 0b11011111
+	// case 6:
+	// 	sv.bits[b] &= 0b10111111
+	// case 7:
+	// 	sv.bits[b] &= 0b01111111
+	// }
 }
